e657a33611776ff4f597613e93a1b05a490890b5
Bruno Alliguieri <alliguierisilva@hotmail.com>
2022-09-21 20:10:36 -0300

    chore: zmodels > models

--- client/client.go
+++ client/client.go
@@ -3,14 +3,14 @@ package zclient
 import (
 	"github.com/megalypse/zhttp"
 	utils "github.com/megalypse/zhttp/internal"
-	"github.com/megalypse/zhttp/zmodels"
+	"github.com/megalypse/zhttp/models"
 )
 
 // Get function that uses predermined data from the given client
 func ClientGet[Response any, Request any](
-	client zmodels.ZClient,
-	request zmodels.ZRequest[Request],
-) zmodels.ZResponse[Response] {
+	client models.ZClient,
+	request models.ZRequest[Request],
+) models.ZResponse[Response] {
 	utils.PrepareClientRequest(&client, &request)
 
 	return zhttp.Get[Response](request)
@@ -18,36 +18,36 @@ func ClientGet[Response any, Request any](
 
 // Post function that makes the request using basic information from the given client
 func ClientPost[Response any, Request any](
-	client zmodels.ZClient,
-	request zmodels.ZRequest[Request],
-) zmodels.ZResponse[Response] {
+	client models.ZClient,
+	request models.ZRequest[Request],
+) models.ZResponse[Response] {
 	utils.PrepareClientRequest(&client, &request)
 
 	return zhttp.Post[Response](request)
 }
 
 func ClientPatch[Response any, Request any](
-	client zmodels.ZClient,
-	request zmodels.ZRequest[Request],
-) zmodels.ZResponse[Response] {
+	client models.ZClient,
+	request models.ZRequest[Request],
+) models.ZResponse[Response] {
 	utils.PrepareClientRequest(&client, &request)
 
 	return zhttp.Patch[Response](request)
 }
 
 func ClientPut[Response any, Request any](
-	client zmodels.ZClient,
-	request zmodels.ZRequest[Request],
-) zmodels.ZResponse[Response] {
+	client models.ZClient,
+	request models.ZRequest[Request],
+) models.ZResponse[Response] {
 	utils.PrepareClientRequest(&client, &request)
 
 	return zhttp.Put[Response](request)
 }
 
 func ClientDelete[Response any, Request any](
-	client zmodels.ZClient,
-	request zmodels.ZRequest[Request],
-) zmodels.ZResponse[Response] {
+	client models.ZClient,
+	request models.ZRequest[Request],
+) models.ZResponse[Response] {
 	utils.PrepareClientRequest(&client, &request)
 
 	return zhttp.Delete[Response](request)
