56599f45666e65fb2a864e6b4bc1b2d22e545c4d
Bruno Alliguieri <alliguierisilva@hotmail.com>
2022-09-27 21:10:22 -0300

    doc: documentation added to ZRequest struct

--- zmodels/z_request.go
+++ zmodels/z_request.go
@@ -1,5 +1,9 @@
 package zmodels
 
+// Request data carrying struct for MakeRequest function.
+// If `IsBytesBody` is set to true, MakeRequest function will expect
+// for the request body to be of type []byte. This is to avoid marshalling
+// and buffering the same bytes array twice.
 type ZRequest[T any] struct {
 	Url         string
 	Headers     map[string]string
